a00fea152598792af8989de6174fcbb109bcebde
Bobinator <69617777+Bobinatorxl@users.noreply.github.com>
2020-09-27 22:17:37 -0700

    Update plugin.py

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -6,7 +6,7 @@ from datetime import datetime
 class allInOne(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.color = 0x000080
+        self.color = 0x0000FF
         self.timer = 10
 
     @tasks.loop(seconds=10)
